3cc97fc70b69346ad838f0fc812a495cedb59534
pedrovgs <pedrovicente.gomez@gmail.com>
2017-02-13 00:25:48 +0100

    Add a new test covering the getCurrentBranch method

--- test/GitSpec.hs
+++ test/GitSpec.hs
@@ -11,20 +11,48 @@ import           SystemFree
 import           Test.Hspec
 import           Test.QuickCheck
 
+spec = describe "Git module requirements" $ do
+  let ?boolInterpreter = run
+  it "returns false if Git is not installed" $ unsafePerformIO $ do
+     let ?systemInterpreter = gitInstalledInterpreter False
+     result <- isGitInstalled
+     return (not result)
+  it "returns true if Git is installed" $ unsafePerformIO $ do
+     let ?systemInterpreter = gitInstalledInterpreter True
+     isGitInstalled
+  it "returns the current branch obtained from the list of branches git returns" $
+    property $ prop_BranchMarkedWithAsteriskRepresentsTheCurrentBranch "any-branch"
+
+prop_BranchMarkedWithAsteriskRepresentsTheCurrentBranch :: String -> Property
+prop_BranchMarkedWithAsteriskRepresentsTheCurrentBranch branch =
+  forAll (repositoryBranches branch) (\genBranches -> unsafePerformIO $ do
+    let ?systemInterpreter = currentBranchInterpreter genBranches
+    result <- getCurrentBranch "anyPath"
+    return (result == branch))
+
+repositoryBranches :: String -> Gen String
+repositoryBranches branch =
+  do position <- arbitrary
+     genBranches <- listOf branchName
+     let currentBranch = "* " ++ branch
+         arrayPosition = abs position `mod` length genBranches
+         branches = insertAt position currentBranch genBranches
+     return (unlines branches)
+
+branchName :: Gen String
+branchName = listOf $ choose ('a', '9')
+
+currentBranchInterpreter :: String -> SystemFreeInterpreter (ExitCode, String, String)
+currentBranchInterpreter branches (Free (Execute command params t)) =
+  run $ t (ExitSuccess, branches, "")
+currentBranchInterpreter _ t = run t
+
 gitInstalledInterpreter :: Bool -> SystemFreeInterpreter (ExitCode, String, String)
 gitInstalledInterpreter result (Free (Execute command params t)) =
   run $ t (errorCode, "", "Command not found")
   where errorCode = if result then ExitSuccess else ExitFailure 9
 gitInstalledInterpreter _ t = run t
 
-
-spec = describe "Git module requirements" $ do
-  it "returns false if Git is not installed" $ unsafePerformIO $ do
-     let ?boolInterpreter = run
-         ?systemInterpreter = gitInstalledInterpreter False
-     result <- isGitInstalled
-     return (not result)
-  it "returns true if Git is installed" $ unsafePerformIO $ do
-     let ?boolInterpreter = run
-         ?systemInterpreter = gitInstalledInterpreter True
-     isGitInstalled
+insertAt :: Int -> a -> [a] -> [a]
+insertAt z y xs = as ++ (y:bs)
+                  where (as,bs) = splitAt z xs
